25d8fab7078f39fdd045c33fdc63c717227f255f
MTRNord <mtrnord1@gmail.com>
2022-03-09 15:33:07 +0100

    Some more lints

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,23 @@
     clippy::inefficient_to_string,
     clippy::macro_use_imports,
     clippy::mut_mut,
-    clippy::todo
+    clippy::todo,
+    clippy::fallible_impl_from,
+    clippy::expl_impl_clone_on_copy,
+    clippy::filetype_is_file,
+    clippy::flat_map_option,
+    clippy::float_cmp,
+    clippy::suboptimal_flops,
+    clippy::unused_self,
+    clippy::unused_async,
+    clippy::fn_to_numeric_cast_any,
+    clippy::if_then_some_else_none,
+    clippy::implicit_hasher,
+    clippy::implicit_saturating_sub,
+    clippy::imprecise_flops,
+    clippy::lossy_float_literal,
+    clippy::map_unwrap_or,
+    clippy::panic_in_result_fn
 )]
 #![warn(missing_docs)]
 #![allow(clippy::missing_panics_doc)]

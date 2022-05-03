841e08b5694a96f6e1c46ab9045b5495313baef0
MTRNord <mtrnord@nordgedanken.dev>
2022-05-03 15:19:59 +0200

    Fix some bugs, add new bugs

--- benches/smtp_bench.rs
+++ benches/smtp_bench.rs
@@ -1,26 +1,36 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::net::TcpStream;
+use erooster::config::Config;
+use std::sync::Arc;
+use std::path::Path;
+use tokio::runtime::Runtime;
+use std::io::{Read, Write};
+use tracing::{info,error};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("login", |b| {
-        info!("Starting ERooster Server");
-        let config = if Path::new("./config.yml").exists() {
-            Arc::new(Config::load("./config.yml").await?)
-        } else if Path::new("/etc/erooster/config.yml").exists() {
-            Arc::new(Config::load("/etc/erooster/config.yml").await?)
-        } else if Path::new("/etc/erooster/config.yaml").exists() {
-            Arc::new(Config::load("/etc/erooster/config.yaml").await?)
-        } else {
-            error!("No config file found. Please follow the readme.");
-            color_eyre::eyre::bail!("No config file found");
-        };
-        erooster::smtp_servers::start(Arc::clone(&config))?;
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            info!("Starting ERooster Server");
+            let config = if Path::new("./config.yml").exists() {
+                Arc::new(Config::load("./config.yml").await.unwrap())
+            } else if Path::new("/etc/erooster/config.yml").exists() {
+                Arc::new(Config::load("/etc/erooster/config.yml").await.unwrap())
+            } else if Path::new("/etc/erooster/config.yaml").exists() {
+                Arc::new(Config::load("/etc/erooster/config.yaml").await.unwrap())
+            } else {
+                error!("No config file found. Please follow the readme.");
+                return;
+            };
+            erooster::smtp_servers::start(config).unwrap();
+        });
         b.iter(|| {
             let mut stream = TcpStream::connect("127.0.0.1:25").unwrap();
 
             //TODO write a message
-            stream.write(&[1])?;
+            stream.write(&[1]).unwrap();
             //TODO verify and continue
-            stream.read(&mut [0; 128])?;
+            stream.read(&mut [0; 128]).unwrap();
         })
     });
 }

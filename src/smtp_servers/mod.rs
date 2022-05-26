0952b62f5c946d168389f4d53093963ea3b2bce4
MTRNord <mtrnord1@gmail.com>
2022-05-26 22:53:32 +0200

    Fix wrong error code expected

--- src/smtp_servers/mod.rs
+++ src/smtp_servers/mod.rs
@@ -246,7 +246,7 @@ pub async fn send_email_job(
                 .await
                 .ok_or("Server did not respond")??;
             debug!("[{}] Got {}", current_job.id(), line);
-            if !line.starts_with("354") {
+            if !line.starts_with("250") {
                 lines_sender.send(String::from("RSET")).await?;
                 lines_sender.send(String::from("QUIT")).await?;
                 return Err("Server did not accept data command".into());

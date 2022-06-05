95e48ad076df197335486e0494d0da6cb941e0af
MTRNord <mtrnord1@gmail.com>
2022-06-05 15:02:46 +0200

    Fix tests

--- crates/erooster_imap/src/commands/capability.rs
+++ crates/erooster_imap/src/commands/capability.rs
@@ -48,7 +48,7 @@ mod tests {
         assert_eq!(
             rx.next().await,
             Some(String::from(
-                "* CAPABILITY AUTH=PLAIN LOGINDISABLED IMAP4rev2 IMAP4rev1"
+                "* CAPABILITY AUTH=PLAIN LOGINDISABLED UTF8=ONLY ENABLE IMAP4rev2 IMAP4rev1"
             ))
         );
     }

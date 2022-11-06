038129cdafcce1590665bab1ca963a1d59f99bc3
MTRNord <mtrnord@nordgedanken.dev>
2022-11-06 17:46:08 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.61.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          erooster = (rustPkgs.workspace.erooster {}).bin;
+          eroosterctl = (rustPkgs.workspace.eroosterctl {}).bin;
+          default = packages.erooster;
+        };
+      }
+    );
+}

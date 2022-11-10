f9845f4421fe6610750f9bbdf4b677637dfb88de
MTRNord <mtrnord@nordgedanken.dev>
2022-11-10 20:35:50 +0100

    Add git to deps

--- flake.nix
+++ flake.nix
@@ -5,25 +5,37 @@
     nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  outputs = inputs: with inputs;
+  outputs = inputs:
+    with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [cargo2nix.overlays.default];
+          overlays = [ cargo2nix.overlays.default ];
         };
 
         rustPkgs = pkgs.rustBuilder.makePackageSet {
           rustVersion = "1.64.0";
           packageFun = import ./Cargo.nix;
+          # Use the existing all list of overrides and append your override
+          packageOverrides = pkgs:
+            pkgs.rustBuilder.overrides.all ++ [
+
+              # parentheses disambiguate each makeOverride call as a single list element
+              (pkgs.rustBuilder.rustLib.makeOverride {
+                name = "vergen";
+                overrideAttrs = drv: {
+                  buildInputs = drv.buildInputs or [ ] ++ [ pkgs.git ];
+                };
+              })
+            ];
         };
 
       in rec {
         packages = {
-          erooster = (rustPkgs.workspace.erooster {});
-          eroosterctl = (rustPkgs.workspace.eroosterctl {});
+          erooster = (rustPkgs.workspace.erooster { });
+          eroosterctl = (rustPkgs.workspace.eroosterctl { });
           default = packages.erooster;
         };
-      }
-    );
+      });
 }

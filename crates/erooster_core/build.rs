d4ee8b0b03f26e73450929cccfa29ecb49f32979
MTRNord <mtrnord@nordgedanken.dev>
2022-11-14 18:00:49 +0100

    Debug flake

--- crates/erooster_core/build.rs
+++ crates/erooster_core/build.rs
@@ -1,6 +1,14 @@
+use std::fs;
+
 use vergen::{vergen, Config, ShaKind};
 
 fn main() -> std::io::Result<()> {
+    let paths = fs::read_dir("./").unwrap();
+
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
+
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     *config.git_mut().skip_if_error_mut() = true;

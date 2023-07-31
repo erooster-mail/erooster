52971efbc4d28b17fed4c0299d2e18e538190e7e
MTRNord <mtrnord1@gmail.com>
2023-07-31 19:54:24 +0200

    Fix syntax errors

--- crates/erooster_core/migrations/20230731143004_scope_uid_to_folder.up.sql
+++ crates/erooster_core/migrations/20230731143004_scope_uid_to_folder.up.sql
@@ -1,10 +1,6 @@
 ALTER TABLE mails
   ADD "uid" BIGINT NOT NULL DEFAULT 0,
-  ADD "mailbox" VARCHAR NOT NULL DEFAULT 'unknown';
-
-ALTER TABLE mails DROP CONSTRAINT mails_pkey;
-
-ALTER TABLE mails ADD PRIMARY KEY ("mailbox", "uid");
+  ADD "mailbox" VARCHAR DEFAULT 'unknown' NOT NULL;
 
 create table IF NOT EXISTS mailbox_uid_counter 
 (
@@ -12,7 +8,7 @@ create table IF NOT EXISTS mailbox_uid_counter
   "mailbox" VARCHAR not null
 );
 
-create function IF NOT EXISTS next_uid_for_folder(m_mailbox VARCHAR) 
+CREATE OR REPLACE function next_uid_for_folder(m_mailbox VARCHAR) 
   returns integer
 as
 $$
@@ -26,7 +22,7 @@ $$
 language sql
 volatile;
 
-create function IF NOT EXISTS increment_uid()
+CREATE OR REPLACE function increment_uid()
   returns trigger
 as
 $$
@@ -37,13 +33,13 @@ end;
 $$
 language plpgsql;
 
-create trigger IF NOT EXISTS base_table_insert_trigger
+CREATE OR REPLACE trigger base_table_insert_trigger
   before insert on mails
   for each row
   execute procedure increment_uid();
 
 
-create trigger IF NOT EXISTS base_table_update_unknown_trigger
+CREATE OR REPLACE trigger base_table_update_unknown_trigger
   before update on mails
   for each row
   when (old.mailbox = 'unknown' AND new.mailbox != 'unknown')

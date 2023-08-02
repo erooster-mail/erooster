127e386cc094b5b9faa61f460a71856ac5c912eb
MTRNord <mtrnord1@gmail.com>
2023-08-02 15:29:54 +0200

    Move to add_trust_anchors

--- crates/erooster_smtp/src/servers/sending.rs
+++ crates/erooster_smtp/src/servers/sending.rs
@@ -495,7 +495,7 @@ async fn get_secure_connection(
     Box<dyn Error + Send + Sync + 'static>,
 > {
     let mut roots = rustls::RootCertStore::empty();
-    roots.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+    roots.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
         OwnedTrustAnchor::from_subject_spki_name_constraints(
             ta.subject,
             ta.spki,

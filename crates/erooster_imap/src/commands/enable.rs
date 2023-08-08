6a08ed1006e171c1a5ec60483db6fb81ee74047a
MTRNord <mtrnord1@gmail.com>
2023-08-08 15:00:36 +0200

    Make sure we always return the capability in an untagged response

--- crates/erooster_imap/src/commands/enable.rs
+++ crates/erooster_imap/src/commands/enable.rs
@@ -25,12 +25,12 @@ impl Enable<'_> {
         for arg in command_data.arguments {
             if arg == &"UTF8=ACCEPT" {
                 write_lock.active_capabilities.push(Capabilities::UTF8);
-                lines.feed(format!("* ENABLED {arg}")).await?;
             } else {
                 write_lock
                     .active_capabilities
                     .push(Capabilities::Other((*arg).to_string()));
             }
+            lines.feed(format!("* ENABLED {arg}")).await?;
         }
         lines.feed(format!("{} OK", command_data.tag)).await?;
         lines.flush().await?;
